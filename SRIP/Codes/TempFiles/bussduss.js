8bd0cb5c1503b37145613773d20c300f74b58eb8
Noobie777 <42256429+Noobie777@users.noreply.github.com>
2019-08-03 21:40:16 +0530

    Add files via upload
    Project completed. Cleaned up the code a bit

--- SRIP/Codes/TempFiles/bussduss.js
+++ SRIP/Codes/TempFiles/bussduss.js
@@ -1,16 +1,22 @@
-
-
 function strip(str) {
     return str.replace(/^\s+|\s+$/g, '');
 }
 function strip2(str){
     return str.replace(/^\s+|\s+$/g,'\t');
 }
+	
+document.addEventListener('DOMContentLoaded',function(){
+	init();
+});
+
+const init = function(e){
+	akhiltopper();
+}
 
 function akhiltopper(){
 	
-	document.getElementById("data_table").setAttribute("style", "visibility:visible;")
-	document.getElementById("text_table").setAttribute("style", "visibility:visible;width:100%; background-color: white;");
+	// document.getElementById("data_table").setAttribute("style", "visibility:visible;")
+	// document.getElementById("text_table").setAttribute("style", "visibility:visible;width:100%; background-color: white;");
 var s = localStorage.getItem('x');
 s=s.split('\n')
 var temp;
@@ -55,25 +61,14 @@ table = document.getElementById('text_table');
  	}
  }
 }
-table1 = document.getElementById('register_table');
- for(var i=0;i<dhoom.length;i++)
- {
-    var newRow=table.insertRow(table.length);
-    for(var j=0;j<dhoom[i].length;j++)
-    {
-        var cell= newRow.insertCell(j);
-        cell.innerHTML=dhoom[i][j];
-    }
- }
-}
 
-function badoom(){
-     var s = document.getElementById("ta").value;
-     localStorage.setItem('x',s);
+// function badoom(){
+//      var s = document.getElementById("ta").value;
+//      localStorage.setItem('x',s);
      
-    // console.log(localStorage.getItem('x'));
-    // document.write(localStorage.getItem('x'));
-     window.location.href="Execute.html";
-    // window.location.assign("pjtry.html");
-     // document.write(localStorage.getItem('x'));   
-}
+//     // console.log(localStorage.getItem('x'));
+//     // document.write(localStorage.getItem('x'));
+//      window.location="pjtry.html";
+//     // window.location.assign("pjtry.html");
+//      // document.write(localStorage.getItem('x'));   
+// }
